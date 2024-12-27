d002e17e3f7c44f254540f6ca6a12cef0a1b6373
Loki <127683587+LokiLeiche@users.noreply.github.com>
2024-12-27 04:19:20 +0100

    Fixed cid generator

--- server/utils.lua
+++ server/utils.lua
@@ -1,8 +1,24 @@
+math.randomseed(os.time())
+
 function Core.Functions.GenerateUID()
     local template = "KC%d%d%d%d%d%d"
-    local nums = ""
-    for i = 1, 6 do
-        nums = nums .. tostring(math.random(0, 9))
+
+    local uid
+    
+    while true do
+        local nums = ""
+        for i = 1, 6 do
+            nums = nums .. tostring(math.random(0, 9))
+        end
+
+        uid = string.format(template, nums:match("(%d)(%d)(%d)(%d)(%d)(%d)"))
+
+        local rs = MySQL.query.await('SELECT citizenid FROM characters WHERE citizenid = ? LIMIT 1', {uid})
+
+        if not rs[1] then
+            break
+        end
     end
-    return string.format(template, nums:match("(%d)(%d)(%d)(%d)(%d)(%d)"))
+
+    return uid
 end
