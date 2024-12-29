afd6aebba89cf3acc0f4603002cceaa1719a8d5d
Kypo <kyper2ndgta@gmail.com>
2024-12-29 09:51:01 +0000

    (client) Discord presence (placeholder)

--- client/functions.lua
+++ client/functions.lua
@@ -14,3 +14,21 @@ function Core.Functions.IsPlayerLoaded()
     return Core.Player.IsLoaded
 end
 
+function DiscordStatus()
+    local playerCount = #GetActivePlayers()
+    local maxPlayers = GetConvarInt('sv_maxclients', 32) -- needs server state
+
+    local presence = {
+        state = "Visit kco.re for more info",
+        details = ("In development | Playing as %s %s"):format(
+            Core.Player.Name.first_name,
+            Core.Player.Name.last_name
+        ),
+        largeImageKey = "kcoreme",
+        largeImageText = "KCore - Visit kco.re for more info.", 
+    }
+    SetDiscordAppId('1322859534003208192')
+    SetDiscordRichPresenceAsset(presence.largeImageKey)
+    SetDiscordRichPresenceAssetText(presence.largeImageText)
+    SetRichPresence(presence.details)
+end
